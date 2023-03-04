1b5082bbd142a452bf60412131cb70f52659c283
Austen Frostad <austen.frostad@intellitect.com>
2023-03-04 09:17:22 -0800

    implement some listmanager methods

---
+++ src/IntelliTect.TextTrolley.Tests/ListToolsTests.cs
@@ -0,0 +1,47 @@
+using IntelliTect.TextTrolley.Web.Utility;
+using OpenAI_API;
+
+namespace IntelliTect.TextTrolley.Tests;
+
+public class ListToolsTests 
+{
+
+    public static IEnumerable<object[]> TestData =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                new List<string>() { "apple", "banana" },
+                new List<string>() { "banana" },
+                new int[] { 1 }
+            },
+            new object[]
+            {
+                new List<string>() { "ice", "ice cream" },
+                new List<string>() { "ice" },
+                new int[] { 0 }
+            },
+            new object[]
+            {
+                new List<string>() { "ice", "ice cream", "shaving cream" },
+                new List<string>() { "cream" },
+                new int[] { 1 }
+            },
+            new object[]
+            {
+                new List<string>() { "ice", "ice cream", "shaving cream", "cream" },
+                new List<string>() { "cream" },
+                new int[] { 3 }
+            },
+        };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GetIndicesInFirstListWhereInSecondList(List<string> list1, List<string> list2, int[] expectedIndices)
+    {
+        List<int> result =  ListTools.GetIndincesToRemove(list1, list2);
+
+        Assert.Equal(expectedIndices, result);
+    }
+}
+
---
+++ src/IntelliTect.TextTrolley.Web/Utility/ListTools.cs
@@ -0,0 +1,24 @@
+﻿using FuzzySharp;
+using OpenAI_API;
+
+namespace IntelliTect.TextTrolley.Web.Utility;
+
+public static class ListTools
+{
+    public static List<int> GetIndincesToRemove( List<string> list1, List<string> list2)
+    {
+        List<int> results = new List<int>();
+
+        foreach (var item in list2)
+        {
+            var found = Process.ExtractOne(item, list1);
+
+            if (found != null && found.Score > 85)
+            {
+                results.Add(found.Index);
+            }
+        }
+
+        return results;
+    }
+}
