9f83cb2118404774489c1cadbdc095af74de6655
Meghan <Meg.Woodford@IntelliTect.com>
2023-03-03 17:42:05 -0800

    Adding color pallet

--- src/IntelliTect.TextTrolley.Web/src/views/Home.vue
+++ src/IntelliTect.TextTrolley.Web/src/views/Home.vue
@@ -2,10 +2,20 @@
   <div class="home">
     <!-- Component is auto-imported by `unplugin-vue-components`
     (either kebab-case or PascalCase works.) -->
+
+    <v-container>
+      <v-alert class="mb-2" type="success" color="primary"> Primary </v-alert>
+      <v-alert class="mb-2" type="success" color="secondary"> Secondary </v-alert>
+      <v-alert class="mb-2" type="success" color="success"> Success </v-alert>
+      <v-alert class="mb-2" type="info" color="info"> Info </v-alert>
+      <v-alert class="mb-2" type="warning" color="warning"> Warning </v-alert>
+      <v-alert class="mb-2" type="error" color="error"> Error </v-alert>
+    </v-container>
+
     <HelloWorld msg="Welcome to your new Vue.js Coalesce application!" />
   </div>
 </template>
 
 <script setup lang="ts">
-useTitle("Home");
+  useTitle("Home");
 </script>
