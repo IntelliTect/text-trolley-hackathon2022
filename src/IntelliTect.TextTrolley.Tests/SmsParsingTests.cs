23189fbb9b41a23bc7be36e32958d0cb8d3d36b9
Austen Frostad <austen.frostad@intellitect.com>
2023-03-03 20:26:57 -0800

    add sms parser

---
+++ src/IntelliTect.TextTrolley.Tests/SmsParsingTests.cs
@@ -0,0 +1,100 @@
+using IntelliTect.TextTrolley.Web.Utility;
+using OpenAI_API;
+
+namespace IntelliTect.TextTrolley.Tests;
+
+public class SmsParsingTests : IClassFixture<SmsParserFixture>
+{
+    SmsParserFixture _Fixture;
+
+    public SmsParsingTests(SmsParserFixture fixture)
+    {
+        this._Fixture = fixture;
+    }
+
+    public static IEnumerable<object[]> TestData =>
+        new List<object[]>
+        {
+            new object[] { "add apple, banana, cherry", 3 },
+            new object[] { "add grape, kiwi, lemon, orange", 4 },
+            new object[] { "please add pear, watermelon", 2 },
+            new object[] { "\"eggs bacon cheese icecream donuts whole milk heavy cream\"", 7 },
+            new object[]
+            {
+                "add to my list fruit\r\nmilk\r\nchips\r\nbread\r\nstuff for dinner\r\n\r\n",
+                5
+            },
+            new object[]
+            {
+                "add to my list 1x dragonfruit\r\n2x quinoa bags\r\na dozen eggz\r\n1 can chickpeaz\r\n1 jar of mermaid sauce\r\n2 lb. grass-fed beaf\r\n1 bundle of unicorn kale\r\n1 loaf of artisanal sourdough bred\r\n1 box of frosted flakz cereal",
+                9
+            }
+        };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public async Task ParseMessageIntoListList_GetsCloseEnough(string list, int expectedCount)
+    {
+        List<string> result = await _Fixture.SmsParser.ParseToItemList(list);
+        if (result.Count < expectedCount)
+        {
+            Assert.Fail(string.Join(", ", result));
+        }
+    }
+
+    public static IEnumerable<object[]> IntentTestData =>
+        new List<object[]>
+        {
+            new object[] { "Add apple, banana, cherry", UserIntent.AddItem },
+            new object[] { "delete grape, kiwi, lemon, orange", UserIntent.RemoveItem },
+            new object[] { "Add pear, watermelon", UserIntent.AddItem },
+            new object[]
+            {
+                "\"eggs bacon cheese icecream donuts whole milk heavy cream\"",
+                UserIntent.AddItem
+            },
+            new object[]
+            {
+                "remove fruit\r\nmilk\r\nchips\r\nbread\r\nstuff for dinner\r\n\r\n",
+                UserIntent.RemoveItem
+            },
+            new object[]
+            {
+                "Please Add 1x dragonfruit\r\n2x quinoa bags\r\na dozen eggz\r\n1 can chickpeaz\r\n1 jar of mermaid sauce\r\n2 lb. grass-fed beaf\r\n1 bundle of unicorn kale\r\n1 loaf of artisanal sourdough bred\r\n1 box of frosted flakz cereal",
+                UserIntent.AddItem
+            },
+            new object[] { "1x dragonfruit\r\n", UserIntent.AddItem },
+            new object[] { "dragonfruit\r\n", UserIntent.AddItem },
+            new object[] { "toilet paper\r\n", UserIntent.AddItem }
+        };
+
+    [Theory]
+    [MemberData(nameof(IntentTestData))]
+    public async Task ParseMessageIntent_GetsCloseEnough(string list, UserIntent userIntent)
+    {
+        var intent = await _Fixture.SmsParser.InterpretUserIntent(list);
+
+        Assert.Equal(userIntent, intent);
+    }
+}
+
+public class SmsParserFixture : IDisposable
+{
+    public ISmsParser SmsParser { get; private set; }
+
+    public SmsParserFixture()
+    {
+        SecretProvider secretProvider = new SecretProvider();
+
+        var apiKey = secretProvider.GetSecret("OpenApiToken").GetAwaiter().GetResult();
+
+        var openai = new OpenAIAPI(apiKey);
+
+        SmsParser = new SmsParser(openai);
+    }
+
+    public void Dispose()
+    {
+        // ... clean up test data from the database ...
+    }
+}
---
+++ src/IntelliTect.TextTrolley.Web/Utility/SmsParser.cs
@@ -0,0 +1,103 @@
+﻿using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace IntelliTect.TextTrolley.Web.Utility;
+
+public class SmsParser : ISmsParser
+{
+    private OpenAIAPI _Client; 
+    // a constructor that gets an OpenAIAPI client from the DI container and assigns it to a field for this class
+    public SmsParser(OpenAIAPI client)
+    {
+        _Client = client;
+    }
+
+    public async  Task<bool> InterpretNegativeResponse(string text)
+    {
+      
+
+        return false;
+    }
+
+    public async Task<bool> InterpretPostiveResponse(string text)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<UserIntent> InterpretUserIntent(string text)
+    {
+        var prompt = """
+            Interpret the intent of the attached message which is from a user who wants to either 
+            1. add items to a grocery list - the message will look like a shopping list or a list of items or just an item, and may have a verb synomous with add
+            2. remove items from a grocery list - the message will look like a shopping list or a list of items or just an item but start with verb synomous with remove
+            3. clear the entire grocery list - the message will be a single word or phrase that is synomous with clear
+            4. or it may be unclear - if you dont understand the message reply return this value
+            Depending on the intent, return the appropriate enum numeric value of the list of options - only the number (no other text or characters).
+            Message below:
+            """;
+
+
+        CompletionRequest completion = new CompletionRequest();
+        completion.Prompt = $"{prompt}\n\n" + string.Join("\n", text);
+        completion.MaxTokens = 100;
+        var result = await _Client.Completions.CreateCompletionAsync(completion);
+
+        if (!result.Completions.Any())
+        {
+
+            throw new Exception("No completions returned");
+        }
+
+        string? enumString = result.Completions[0].Text;
+
+        // remove the newline characters from the text
+        enumString = enumString.Replace("\n", "").Replace("\r", "");
+
+        // get the first number character in the string
+        int firstNumberIndex = enumString.IndexOfAny("1234".ToCharArray());
+
+        if (firstNumberIndex < 0) {
+            return UserIntent.Unknown;        
+        }
+
+
+        // get the char at the first number index and convert it to an UserIntent
+        return (UserIntent)int.Parse(enumString[firstNumberIndex].ToString());
+     
+
+    }
+
+    public async Task<List<string>> ParseToItemList(string text)
+    {
+
+        var prompt = """
+            Parse the attached message which is a grocery list using following rules.
+            Ignore any starting text that is about the list, like "grocery list" or "please", or "here is my list" or "add to my list"
+            Spell check any items that you are 100 percent are spelled wrong.
+            Also please retain any quanitity information, as well as descriptions about like the size of the item, packaging, or container.
+            Format the response as single string with items seperated by commas
+            if there are commas in the original text, remove them.
+            Message below:
+            """;
+
+        CompletionRequest completion = new CompletionRequest();
+        completion.Prompt = $"{prompt}\n\n" + string.Join("\n", text);
+        completion.MaxTokens = text.Length+100;
+        var result = await _Client.Completions.CreateCompletionAsync(completion);
+
+        if (!result.Completions.Any()) {
+        
+            throw new Exception("No completions returned");
+        }
+
+        string? parsedList = result.Completions[0].Text;
+
+        
+
+        // remove the newline characters from the text
+        parsedList = parsedList.Replace("\n","").Replace("\r","");
+
+        // split the result into a list of strings and return it
+        return parsedList.Split(",").ToList();
+    }
+}
